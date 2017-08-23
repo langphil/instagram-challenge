31186880dec7204e21e1b752cdb1aa85e0b2ce49
Phil Lang <plangphoto@gmail.com>
2017-08-23 23:58:19 +0100

    User: sign up, sign in, sign out, edit account, delete account, rpsec tests - styling still required

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 0) do
-=======
 ActiveRecord::Schema.define(version: 20170823213146) do
->>>>>>> users
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-=======
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,6 +34,4 @@ ActiveRecord::Schema.define(version: 20170823213146) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
->>>>>>> users
 end
