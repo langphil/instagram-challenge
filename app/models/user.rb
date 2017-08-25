699b4bd4389efc4c1936dbc2068cdc6237947cbb
Phil Lang <plangphoto@gmail.com>
2017-08-25 06:19:34 +0100

    User signup validations - username, email, password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :user_name, :first_name, :last_name, :email, :password, presence: true
+  validates :user_name, :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
 end
