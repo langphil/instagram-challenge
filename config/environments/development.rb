bafd4ba05712f0dee4dcee10c67622aaa8e96ba6
Phil Lang <plangphoto@gmail.com>
2017-08-26 13:14:06 +0100

    Heroku configuration set in production and development environments - credentials to be extracated and hidden before github push

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,15 @@ Rails.application.configure do
 
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('instaphil'),
+      access_key_id: ENV.fetch('AKIAJZBQHCWPGLPGLPYA'),
+      secret_access_key: ENV.fetch('sUnItvLTyoiv3S0fyOTo2tTYaczB2pWhBNhP+q58'),
+      s3_region: ENV.fetch('s3.eu-west-2.amazonaws.com'),
+    }
+  }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
